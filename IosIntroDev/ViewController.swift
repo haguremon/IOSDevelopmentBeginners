422804f7dfa0b9ea802a2a96ca5d712edd4cd04e
Yuta iwasaki <yanyong0127@gmail.com>
2021-06-24 13:56:02 +0900

    これからXcodeでの作業に入るー

--- IosIntroDev/ViewController.swift
+++ IosIntroDev/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
+    
+    @IBOutlet weak var buttonTaptextFieid: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func pushButton(_ sender: Any) { //pushButtonがタップされた時にhelloをhelloLabelに表示する
+        guard let textFieid = buttonTaptextFieid.text else {
+            return
+        }//if let でやると分岐することができるなので値がない時に　デフォルト値を設定することもできる //textFieidの値があるだけで表示されるにnilは許されない
+        helloLabel.text = textFieid
+       
+    }
+    
 
 }
 
