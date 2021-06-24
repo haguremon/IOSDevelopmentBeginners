6f4b2a6d92163ac369bc70157863966a85d8ae29
Yuta iwasaki <yanyong0127@gmail.com>
2021-06-24 18:26:40 +0900

    "簡潔にした"

--- IosIntroDev/ViewController.swift
+++ IosIntroDev/ViewController.swift
@@ -5,25 +5,29 @@
 //  Created by IwasakIYuta on 2021/06/23.
 //test
 
-import UIKit
+import UIKit //モージュル（UIKitという名前のフレームワーク（クラス群）を使えるようにするApple社ありがたや
 
+//   ViewController(サブクラス): UIViewController(スーパークラス)
 class ViewController: UIViewController {
 
-    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var text1Label: UILabel!
     
+    @IBOutlet weak var defaultHellotext2Label: UILabel!
     @IBOutlet weak var buttonTaptextFieid: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //view.addSubview(helloLabel)
+        // defaultHellotext2Label.text = "HELLO"
     }
     
     @IBAction func pushButton(_ sender: Any) { //pushButtonがタップされた時にhelloをhelloLabelに表示する
-        guard let textFieid = buttonTaptextFieid.text else {
-            return
-        }//if let でやると分岐することができるなので値がない時に　デフォルト値を設定することもできる //textFieidの値があるだけで表示されるにnilは許されない
-        helloLabel.text = textFieid
-       
+        text1Label.text = buttonTaptextFieid.text
+        defaultHellotext2Label.text = buttonTaptextFieid.text
+        if defaultHellotext2Label.text?.isEmpty == true {
+            defaultHellotext2Label.text = "hello"
+        }
+    
     }
     
 
