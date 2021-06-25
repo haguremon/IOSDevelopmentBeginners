367bdf90f4863dc37fe9622fdafc6ba12d136bac
Yuta iwasaki <yanyong0127@gmail.com>
2021-06-25 10:54:20 +0900

    UIsliderを追加

--- IosIntroDev/ViewController.swift
+++ IosIntroDev/ViewController.swift
@@ -15,13 +15,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var defaultHellotext2Label: UILabel!
     @IBOutlet weak var buttonTaptextFieid: UITextField!
     
+    @IBOutlet weak var sliderLabel: UILabel!
+    //6/25スライダーの値を表示するラベルを追加
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //view.addSubview(helloLabel)
         // defaultHellotext2Label.text = "HELLO"
     }
     
-    @IBAction func pushButton(_ sender: Any) { //pushButtonがタップされた時にhelloをhelloLabelに表示する
+    @IBAction func pushButton(_ sender: Any) {
+        //pushButtonがタップされた時にhelloをhelloLabelに表示する
         text1Label.text = buttonTaptextFieid.text
         defaultHellotext2Label.text = buttonTaptextFieid.text
         if defaultHellotext2Label.text?.isEmpty == true {
@@ -29,7 +33,10 @@ class ViewController: UIViewController {
         }
     
     }
+    //sliderを移動するときにsliderLabelに値を表示する機能追加
+    @IBAction func sliderTap(_ sender: UISlider) {
+        sliderLabel.text = "\(sender.value)"
     
-
+    }
 }
 
